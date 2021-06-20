d1ebb7b6ec5e0689ffa0f08721a66c578645b9a9
ele <2727262253@qq.com>
2021-06-20 17:32:56 +0800

    Init starter

---
+++ walnut-starter/src/test/java/io/eele/walnut/WalnutStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eele.walnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalnutStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
